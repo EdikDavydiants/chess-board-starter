abdc98fba43aba17fe9b892ff2c0623e777e8ace
Edik <davydiantsedik@mail.ru>
2024-12-15 13:54:31 +0300

    Root

---
+++ src/main/java/ed/d/chessboard/ChessboardApplication.java
@@ -0,0 +1,13 @@
+package ed.d.chessboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ed/d/chessboard/ChessboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ed.d.chessboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
